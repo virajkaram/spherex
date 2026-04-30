8d69f76dd56c3a89ccfdc6819abe6840b5db4b08
viraj21197 <karambelkarvraj21197@gmail.com>
2026-04-30 18:54:10 -0400

    pop

--- spherex/parse_headers.py
+++ spherex/parse_headers.py
@@ -38,10 +38,6 @@ def get_record_from_file(file_path: str | Path):
     new_values.pop('COMMENT', None)  # Remove COMMENT if it exists
     new_values.pop('HISTORY', None)  # Remove HISTORY if it exists
 
-    # pop l2_n_ghost, l2_n_ghost_fpa, l2_n_snow_ns, refcat.version, astronetsolved, qs_nmatch_var, match_nstars, daxis, nhalopix, contrast, l2_n_tran_nb, nblmpix, l2_n_ghost_ext, l2_n_pers_nb, l2_n_bloom_ns, nsnowpix, maxblmlp
-    for key in ['l2_n_ghost', 'l2_n_ghost_fpa', 'l2_n_snow_ns', 'refcat.version', 'astronetsolved', 'qs_nmatch_var', 'match_nstars', 'daxis', 'nhalopix', 'contrast', 'l2_n_tran_nb', 'nblmpix', 'l2_n_ghost_ext', 'l2_n_pers_nb', 'l2_n_bloom_ns', 'nsnowpix', 'maxblmlp']:
-        new_values.pop(key, None)
-        
     # Replace - by _ in keys
     for key in list(new_values.keys()):
         new_key = key
@@ -53,6 +49,10 @@ def get_record_from_file(file_path: str | Path):
     new_values["footprint"] = footprint_polygon
     new_values["savepath"] = file_path.as_posix()
     new_values["qr_version"] = LATEST_QR_VERSION
+
+    # pop l2_n_ghost, l2_n_ghost_fpa, l2_n_snow_ns, refcat.version, astronetsolved, qs_nmatch_var, match_nstars, daxis, nhalopix, contrast, l2_n_tran_nb, nblmpix, l2_n_ghost_ext, l2_n_pers_nb, l2_n_bloom_ns, nsnowpix, maxblmlp
+    for key in ['l2_n_ghost', 'l2_n_ghost_fpa', 'l2_n_snow_ns', 'refcat.version', 'astronetsolved', 'qs_nmatch_var', 'match_nstars', 'daxis', 'nhalopix', 'contrast', 'l2_n_tran_nb', 'nblmpix', 'l2_n_ghost_ext', 'l2_n_pers_nb', 'l2_n_bloom_ns', 'nsnowpix', 'maxblmlp']:
+        new_values.pop(key, None)
 
     image_hdulist.close()
     return new_values
